4d5d32c3fc22e6fcdc55b2f39daab63b06cbf9b0
water2022 <97374665+water2022@users.noreply.github.com>
2024-12-21 21:20:41 +0800

    Add files via upload

---
+++ my-scripts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 执行第一个命令
+bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t c4f99c5e-cc23-495f-9430-ceb39869b5a3 -u https://ny.sulian.cc"
+
+# 执行第二个命令
+bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 993c6e40-a718-4187-a88f-d3148ead9c61 -u https://kk.zhongzhuan123.buzz"
